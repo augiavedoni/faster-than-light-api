5b0ade82163e717c686e51661dbc54ea33a879f5
Augusto Giavedoni <58192738+augiavedoni@users.noreply.github.com>
2022-05-27 22:46:17 +0200

    Change incorrect import

--- src/test/java/com/codesherpas/ftl/PowerGeneratorServiceTests.java
+++ src/test/java/com/codesherpas/ftl/PowerGeneratorServiceTests.java
@@ -1,6 +1,6 @@
 package com.codesherpas.ftl;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Assertions;
